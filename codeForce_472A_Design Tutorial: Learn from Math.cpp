01c248843dc63c1425a281d443372648078aad8e
Rukaiya Fahmida <rukaiyafahmida7@gmail.com>
2021-03-20 22:27:11 +0600

    Create codeForce_472A_Design Tutorial: Learn from Math.cpp

---
+++ codeForce_472A_Design Tutorial: Learn from Math.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#include <string>
+#include <string.h>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <utility>
+using namespace std;
+
+
+int main()
+{
+    int x;
+    cin>>x;
+    if(x%2==0)
+    {
+        cout<<4<<" "<<x-4<<endl;
+    }
+    else
+    {
+        cout<<9<<" "<<x-9<<endl;
+    }
+}
+

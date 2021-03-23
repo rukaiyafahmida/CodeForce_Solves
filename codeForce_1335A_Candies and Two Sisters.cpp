dc2e4e2b5d2783c626c15bd066b16ac483c5d84d
Rukaiya Fahmida <rukaiyafahmida7@gmail.com>
2021-03-23 23:46:39 +0600

    Create codeForce_1335A_Candies and Two Sisters.cpp

---
+++ codeForce_1335A_Candies and Two Sisters.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <cstring>
+#include<stdlib.h>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    long int n,r=0;
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        cin>>n;
+        if(n%2 == 0)
+        {
+            cout<<(n/2-1)<<endl;
+        }
+        else
+            cout<<n/2<<endl;
+    }
+
+    return 0;
+}

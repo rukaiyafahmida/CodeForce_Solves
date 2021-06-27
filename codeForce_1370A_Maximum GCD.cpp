db7ae9125b4df02967d5fdacae97894dd30d6ed8
Rukaiya Fahmida <rukaiyafahmida7@gmail.com>
2021-06-28 02:01:58 +0600

    Create codeForce_1370A_Maximum GCD.cpp

---
+++ codeForce_1370A_Maximum GCD.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include <string>
+#include <string.h>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <utility>
+#include <cstdlib>
+
+using namespace std;
+
+
+int main()
+{
+    long int n,x,t;
+    cin>>t;
+    for(int i=0; i<t; i++)
+    {
+
+        cin >> n;
+
+        x=floor(n/2);
+
+        cout<<x<<endl;
+
+
+    }
+}
+
+//one of my biggest flex
